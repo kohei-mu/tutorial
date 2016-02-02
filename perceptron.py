6c8950e4ea55b573d1a22fbd525f95ac75147b87
kohei-mu <kohei-mu@muramatsukouhei-no-MacBook-Air.local>
2016-02-03 00:35:30 +0900

    modified

--- perceptron.py
+++ perceptron.py
@@ -5,44 +5,53 @@
 #http://www.phontron.com/slides/nlp-programming-ja-03-perceptron.pdf
 
 #f = open("../Downloads/nlptutorial/test/03-train-input.txt","r").readlines()
-f = open("../Downloads/nlptutorial/data/titles-en-train.labeled","r").readlines()
-j = open("../Downloads/nlptutorial/data/titles-en-test.word","r").readlines()
-
+f = open("../../Downloads/nlptutorial/data/titles-en-train.labeled","r").readlines()
+j = open("../../Downloads/nlptutorial/data/titles-en-test.word","r").readlines()
+from collections import defaultdict
 #最初の重みを設定
 def first_weights(lines):
-	weight = {}
-	for i in lines:
-		i = i.rstrip()
-		i = i.split("\t")
-		x = i[1]
-		words = x.split(" ")
-		for word in words:
-			word = "UNI: "+word
-			if word in weight:
-				weight[word] += 0
-			else:
-				weight[word] = 0
-	return weight
+    weight = defaultdict(int)
+    for i in lines:
+        i = i.strip().split("\t")
+        words = i[1].split(" ")
+        for word in words:
+            word = "UNI: "+word
+            weight[word] = 0
+    return weight
 
-#ユニグラムの素性を抽出
+#ユニグラムの素性を抽出c ../
 def create_features(lines):
-	phi = {}
-	i = lines.rstrip()
-	words = i.split(" ")
+	#phi = {}
+	phi = defaultdict(int)
+	#i = lines.strip()
+	#words = i.split(" ")
+	words = lines.strip().split(" ")
 	for word in words:
 		word = "UNI: "+word
-		if word in phi:
-			phi[word] += 1
-		else:
-			phi[word] = 1
+		phi[word] += 1
 	return phi
+
+#def create_features_(lines):
+#    phi = defaultdict(int)
+#    counts = defaultdict(int)
+#    total_count = 0
+
+#    words = lines.strip().split(" ")
+#    for word in words:
+#        word = "UNI: " + word
+#        counts[word] += 1
+#        total_count += 1
+#    for word in words:
+#        word = "UNI: " + word
+#        phi[word] = float(counts[word]) / total_count
+#    return phi
 
 #一行ごとの重み付き和を計算し、予測
 def predict_one(weight, phi):
 	score = 0
 	for name,value in phi.items():
 		if name in weight:
-			score += value * weight[name]
+			score += float(value) * weight[name]
 	if score >= 0:
 		return 1
 	else:
@@ -73,15 +82,14 @@ def learn(input):
 #全体的な予測
 def predict_all(input, test):
 	weight = learn(input)
+	print weight
 	for i in test:
 		i = i.rstrip()
 		phi = create_features(i)
+		#print phi
 		y_ = predict_one(weight,phi)
-		print y_,i
+		#print y_,i
 
 
 #実行
 predict_all(f,j)
-
-
-
